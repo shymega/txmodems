4e9ec0c0cc9c512da5b5f9133f51d30244096ce7
Dom Rodriguez <shymega@shymega.org.uk>
2021-05-02 21:24:03 +0100

    Make consts public
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/consts/ymodem.rs
+++ src/consts/ymodem.rs
@@ -1,17 +1,17 @@
 //! Collection of protocol bytes for internal usage in `txmodems` (YMODEM-specific)
 #![allow(dead_code)] // temporary
 
-const NUL: &[u8] = b"\x00";
-const SOH: &[u8] = b"\x01";
-const STX: &[u8] = b"\x02";
-const EOT: &[u8] = b"\x04";
-const ACK: &[u8] = b"\x06";
-const ACK2: &[u8] = b"\x86";
-const DLE: &[u8] = b"\x10";
-const NAK: &[u8] = b"\x15";
-const CAN: &[u8] = b"\x18";
-const CAN2: &[u8] = b"\x98";
-const CRC: &[u8; 1] = b"C";
-const CRC2: &[u8] = b"\xc3";
-const CRC3: &[u8] = b"\x83";
-const ABT: &[u8; 1] = b"a";
+pub const NUL: &[u8] = b"\x00";
+pub const SOH: &[u8] = b"\x01";
+pub const STX: &[u8] = b"\x02";
+pub const EOT: &[u8] = b"\x04";
+pub const ACK: &[u8] = b"\x06";
+pub const ACK2: &[u8] = b"\x86";
+pub const DLE: &[u8] = b"\x10";
+pub const NAK: &[u8] = b"\x15";
+pub const CAN: &[u8] = b"\x18";
+pub const CAN2: &[u8] = b"\x98";
+pub const CRC: &[u8; 1] = b"C";
+pub const CRC2: &[u8] = b"\xc3";
+pub const CRC3: &[u8] = b"\x83";
+pub const ABT: &[u8; 1] = b"a";
